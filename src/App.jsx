0bf4c670cfb3f3ce6e0caa1386537afe5e9ea710
MckenzieGolshani <mrg140@uakron.edu>
2025-05-19 15:41:18 -0400

    Implement App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,26 @@
 // App.jsx
+
+import { onAuthStateChanged, signOut } from 'firebase/auth';
+import { useEffect, useState } from 'react';
+import { auth } from './firebase';
+import AuthPage from './AuthPage';
+
+function App() {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const unsub = onAuthStateChanged(auth, setUser);
+    return () => unsub();
+  }, []);
+
+  if (!user) return <AuthPage />;
+
+  return (
+    <div>
+      <p>Welcome, {user.email}</p>
+      <button onClick={() => signOut(auth)}>Log Out</button>
+    </div>
+  );
+}
+
+export default App;
